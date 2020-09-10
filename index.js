fa9a51ab1265039956b166381707c2e5e6b2c3ed
rabiulalam1 <rabiul.alam@outlook.com>
2020-09-10 14:57:32 -0400

    Heroku link added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const BG_COLOR = "#231f20";
 const SNAKE_COLOR = "#c2c2c2";
 const FOOD_COLOR = "#e66916";
 
-const socket = io("https://obscure-citadel-40924.herokuapp.com/");
+const socket = io("https://obscure-citadel-40924.herokuapp.com");
 
 socket.on("init", handleInit);
 socket.on("gameState", handleGameState);
